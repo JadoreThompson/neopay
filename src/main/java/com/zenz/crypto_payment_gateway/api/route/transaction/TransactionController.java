98c9aed5402301beda7ad28a7cf0f65604755e3f
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-13 10:44:10 +0000

    wip: Created CRUD APIs (transaction api) - Implemented TransactionController

--- src/main/java/com/zenz/crypto_payment_gateway/api/route/transaction/TransactionController.java
+++ src/main/java/com/zenz/crypto_payment_gateway/api/route/transaction/TransactionController.java
@@ -1,19 +1,45 @@
 package com.zenz.crypto_payment_gateway.api.route.transaction;
 
 import com.zenz.crypto_payment_gateway.api.route.transaction.response.TransactionResponse;
+import com.zenz.crypto_payment_gateway.entity.Transaction;
+import com.zenz.crypto_payment_gateway.entity.User;
+import com.zenz.crypto_payment_gateway.service.MerchantService;
+import com.zenz.crypto_payment_gateway.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/transaction/")
+@RequestMapping("/merchants/{merchantId}/transactions/")
+@RequiredArgsConstructor
 public class TransactionController {
-    @GetMapping("/{transactionId}/")
-    public TransactionResponse getTransaction(@PathVariable String transactionId) {return null;}
+
+    private final TransactionService transactionService;
+    private final MerchantService merchantService;
 
     @GetMapping("/")
-    public List<TransactionResponse> getTransactions() {return null;}
+    public ResponseEntity<List<TransactionResponse>> getTransactions(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID merchantId,
+            @RequestParam(value = "invoiceId") UUID invoiceId
+    ) {
+        merchantService.getMerchantByIdAndUserId(merchantId, user.getUserId());
+        List<Transaction> transactions = transactionService.getTransactionsByInvoiceId(invoiceId);
+        return ResponseEntity.ok(transactions.stream().map(transactionService::toResponse).toList());
+    }
 
-    @GetMapping("/invoice/{invoiceId}/")
-    public List<TransactionResponse> getTransactionsByInvoice(@PathVariable String invoiceId) {return null;}
+    @GetMapping("/{transactionId}/")
+    public ResponseEntity<TransactionResponse> getTransaction(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID merchantId,
+            @PathVariable UUID transactionId
+    ) {
+        merchantService.getMerchantByIdAndUserId(merchantId, user.getUserId());
+        Transaction transaction = transactionService.getTransactionById(transactionId);
+        return ResponseEntity.ok(transactionService.toResponse(transaction));
+    }
 }
